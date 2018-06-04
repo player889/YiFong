66da77d75c8e2d69e60f85bb1d87cc89ed88343c
Jay <hsiao889@gmail.com>
2018-06-04 16:16:45 +0800

    remove auto database connection setting

--- yifong-web/src/main/java/com/company/yifong/ControllerApplication.java
+++ yifong-web/src/main/java/com/company/yifong/ControllerApplication.java
@@ -1,14 +1,19 @@
 package com.company.yifong;
 
 import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.company.yifong", "com.comany.yifong.auth", "com.comany.yifong.security", "com.comany.yifong.exception", "com.comany.yifong.controller",
-		"com.comany.yifong.service", "com.comany.yifong.dao" })
+@ComponentScan(basePackages = { "com.company.yifong", "com.comany.yifong.auth", "com.comany.yifong.security",
+		"com.comany.yifong.exception", "com.comany.yifong.controller", "com.comany.yifong.service",
+		"com.comany.yifong.dao" })
+// Stop initial database setting
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class ControllerApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
@@ -16,8 +21,6 @@ public class ControllerApplication extends SpringBootServletInitializer {
 	}
 
 	public static void main(final String[] args) {
-		new SpringApplicationBuilder()
-				.bannerMode(Banner.Mode.OFF).sources(ControllerApplication.class)
-				.run(args);
+		new SpringApplicationBuilder().bannerMode(Banner.Mode.OFF).sources(ControllerApplication.class).run(args);
 	}
 }
