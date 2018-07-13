2d35c130e2582e7f40f78e81f7ef267d98713152
Jay <hsiao889@gmail.com>
2018-07-13 13:55:16 +0800

    order html

--- yifong-web/src/main/resources/static/js/order/order.js
+++ yifong-web/src/main/resources/static/js/order/order.js
@@ -1,7 +1,7 @@
 class Order {
 	constructor() {
 		this.initEvent();
-		this.initData()
+		this.initData();
 	}
 	initData(){
 		this._areaCharge = ["基隆", "基隆-台中", "基隆-彰化", "台北港", "桃園", "觀音", "楊梅", "湖口", "台中", "台中-銅鑼", "台中-彰化", "嘉義", "高雄"];
@@ -19,14 +19,39 @@ class Order {
 		});
 
 		let self = this;
-		$('#client').on('change', function () {
-			commonUtils.doAjax('/order/client/charges', { no: $('#client').val() }, function (resp) {
-				let usedArea = _.map(resp.data, function(currentObject) {
-					return parseInt(currentObject.dest);
-				});
-				usedArea.forEach(function(item){
-					console.log(self._areaCharge[item]);
-				});
+		$('#info-client').on('change', function () {
+			commonUtils.doAjax('/order/client/charges', { no: $('#info-client').val() }, function (resp) {
+				
+//				console.log(resp.data);
+//				
+//				self._size = _.map(resp.data, function(currentObject) {
+//					return currentObject.size;
+//				});
+//				
+//				 $('#size > option').each(function(){
+//					 let isDsiabled = self._size.includes(parseInt($(this).val())) ? false : true;
+//					 $(this).prop("disabled" , isDsiabled);
+//				 });
+//				 $('#size').selectpicker({
+//					hideDisabled: true
+//				});
+//				 $('#size').selectpicker('refresh');
+//				 $("#size").selectpicker("val", $("#size option:enabled:first").val());
+//				
+//				let obj = $('#area');
+//				if(false === $.isEmptyObject(resp.dat)){
+//					resp.data.forEach(function(item){
+//						obj.append('<option value="'+item.dest+'">'+self._areaCharge[item.dest]+'</option');
+//					});
+//					
+//					obj.on("change", function(){
+//						let o = resp.data[$(this).prop('selectedIndex') - 1];
+//						$("#pay").val(o.pay);
+//						$("#lyfee").val(o.fee);
+//					});
+//					$('#area').selectpicker('refresh');
+//				}
+			
 			});
 		});
 
@@ -44,6 +69,11 @@ class Order {
 }
 let o = new Order();
 
+
+//validator = new ContainerValidator();
+//console.log(validator.isValid('TEXU3070079'));
+//console.log(validator.isValid('TEXU3070070'));
+//console.log(validator.isValid('YMLU8305416'));
 
 // if(undefined != resp.data){
 // let usedArea = _.map(resp.data, function(currentObject) {
