45978e85b85b83a88d656aded8c0f77c04061bdd
Jay <hsiao889@gmail.com>
2018-07-29 01:06:05 +0800

    js

--- yifong-web/src/main/resources/static/js/content/client.js
+++ yifong-web/src/main/resources/static/js/content/client.js
@@ -26,9 +26,6 @@
 			
 			var self = this;
 
-			var header = $("meta[name='_csrf_header']").attr("content");
-			var token = $("meta[name='_csrf']").attr("content");
-
 			return $("#example").DataTable({
 				"ajax": {
 					"contentType": 'application/json',
